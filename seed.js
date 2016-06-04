98e41691876f25948eacd062884ad507a4e0f906
Jonah Wolf <wolfjonah@gmail.com>
2016-06-04 18:58:24 -0400

    Seeds one project with user and datasource

--- seed.js
+++ seed.js
@@ -17,48 +17,88 @@ name in the environment files.
 
 */
 
+var fs = require('fs');
 var mongoose = require('mongoose');
 var Promise = require('bluebird');
 var chalk = require('chalk');
 var connectToDb = require('./server/db');
 var User = mongoose.model('User');
+var DataSource = mongoose.model('DataSource');
+var Project = mongoose.model('Project');
 
-var wipeCollections = function () {
-    var removeUsers = User.remove({});
-    return Promise.all([
-        removeUsers
-    ]);
+var wipeCollections = function() {
+  var removeUsers = User.remove({});
+  return Promise.all([
+    removeUsers
+  ]);
 };
 
-var seedUsers = function () {
+var seedUsers = function() {
 
-    var users = [
-        {
-            email: 'testing@fsa.com',
-            password: 'password'
-        },
-        {
-            email: 'obama@gmail.com',
-            password: 'potus'
-        }
-    ];
+  var users = [{
+    email: 'testing@fsa.com',
+    password: 'password'
+  }, {
+    email: 'obama@gmail.com',
+    password: 'potus'
+  }];
 
-    return User.create(users);
+  return User.create(users);
 
+};
+
+var seedDataSource = function() {
+  return new Promise(function(fulfill, reject) {
+      fs.readFile('./iris.json', 'utf8', function(err, res) {
+        if (err) reject(err);
+        else fulfill(res);
+      });
+    })
+    .then(function(err, contents) {
+      var dataSource = {
+        fileName: 'iris',
+        dataType: 'linear',
+        data: contents
+      };
+      return DataSource.create(dataSource);
+    });
+};
+
+var seedProject = function() {
+  return User.find({ email: 'obama@gmail.com' })
+    .then(function(obama) {
+      return DataSource.findOne({ fileName: 'iris' })
+        .then(function(iris) {
+          return {
+            name: 'Obama\s Iris',
+            user: obama._id,
+            dataSource: iris._id
+          };
+        });
+    })
+    .then(function(project) {
+      return Project.create(project);
+    });
 };
 
 connectToDb
-    .then(function () {
-        return wipeCollections();
-    })
-    .then(function () {
-        return seedUsers();
-    })
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.kill(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.kill(1);
-    });
+  .then(function() {
+    return wipeCollections();
+  })
+  .then(function() {
+    return seedUsers();
+  })
+  .then(function() {
+    return seedDataSource();
+  })
+  .then(function() {
+    return seedProject();
+  })
+  .then(function() {
+    console.log(chalk.green('Seed successful!'));
+    process.kill(0);
+  })
+  .catch(function(err) {
+    console.error(err);
+    process.kill(1);
+  });
