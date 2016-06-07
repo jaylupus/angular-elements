4f901be7117bcc03026423542d6f438f7e0a2f96
Jonah Wolf <wolfjonah@gmail.com>
2016-06-07 18:04:20 -0400

    Editor directive inherits from parent scope

--- browser/js/common/directives/editor/editor.js
+++ browser/js/common/directives/editor/editor.js
@@ -1,25 +1,6 @@
-app.controller('FormController', function($rootScope, $scope, EditService) {
-  $scope.schema = EditService.getSchema('soloTable');
- 
-  $scope.form = [
-    "*",
-    {
-      type: "submit",
-      title: "Save"
-    }
-  ];
- 
-  $scope.model = {};
-
-  $rootScope.$on('select', function(args){
-  	$scope.schema = args.schema;
-  	$scope.model = args.model;
-  });
-
-})
-.directive('editor', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'js/common/directives/editor/editor.html'
-	};
+app.directive('aiEditor', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/common/directives/editor/editor.html'
+  };
 });
