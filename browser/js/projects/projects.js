4e6168a1a447da7eb83c4b035a279937eb1ca36a
andrewmenendez <andrew@engrade.com>
2016-06-09 17:15:51 -0400

    added script tags to index

--- browser/js/projects/projects.js
+++ browser/js/projects/projects.js
@@ -1,92 +1,92 @@
-app.config(function($stateProvider) {
-  $stateProvider.state('project', {
-    url: '/project/:id',
-    templateUrl: '/js/projects/project.edit.html',
-    controller:'ProjectEditCtrl',
-    resolve: {
-      project: function(ProjectFactory,$stateParams) {
-        return ProjectFactory.getOne($stateParams.id);
-      }
-    }
-  })
-});
+// app.config(function($stateProvider) {
+//   $stateProvider.state('project', {
+//     url: '/project/:id',
+//     templateUrl: '/js/projects/project.edit.html',
+//     controller:'ProjectEditCtrl',
+//     resolve: {
+//       project: function(ProjectFactory,$stateParams) {
+//         return ProjectFactory.getOne($stateParams.id);
+//       }
+//     }
+//   })
+// });
 
-app.controller('ProjectEditCtrl', function($scope,project){
-  $scope.project=project;
-  $scope.rows=project.config[0].pages.page_1.rows
-
-
+// app.controller('ProjectEditCtrl', function($scope,project){
+//   $scope.project=project;
+//   $scope.rows=project.config[0].pages.page_1.rows
 
 
 
-})
 
-app.factory('ProjectFactory', function($http) {
-  var projectObj;
-  var _projectCache = [];
 
-  projectObj = {
-    getAll: function() {
-      return $http.get('/api/projects')
-        .then(function(projects) {
-          console.log(projects);
-          angular.copy(projects.data, _projectCache);
-          return _projectCache;
-        });
-    },
+// })
 
-    getAllByUser: function(userId){
-      return $http.get('/api/projects/user/' + userId)
-          .then(function(projects){
-            console.log(projects);
-            angular.copy(projects.data, _projectCache);
-            return _projectCache;
-          })
-    },
+// app.factory('ProjectFactory', function($http) {
+//   var projectObj;
+//   var _projectCache = [];
 
-    getOne: function(id) {
-      return $http.get('/api/projects/' + id)
-        .then(function(project) {
-          return project.data;
-        });
-    },
+//   projectObj = {
+//     getAll: function() {
+//       return $http.get('/api/projects')
+//         .then(function(projects) {
+//           console.log(projects);
+//           angular.copy(projects.data, _projectCache);
+//           return _projectCache;
+//         });
+//     },
 
-    add: function(project) {
-      return $http({
-            url: '/api/projects/',
-            method: "POST",
-            data: project
-      })
-        .then(function(_project) {
-          return _project.data;
-        });
-    },
+//     getAllByUser: function(userId){
+//       return $http.get('/api/projects/user/' + userId)
+//           .then(function(projects){
+//             console.log(projects);
+//             angular.copy(projects.data, _projectCache);
+//             return _projectCache;
+//           })
+//     },
 
-    delete: function(id){
-      return $http.delete('/api/projects/' + id)
-        .then(function(project) {
-          return project.data;
-        });
-    },
+//     getOne: function(id) {
+//       return $http.get('/api/projects/' + id)
+//         .then(function(project) {
+//           return project.data;
+//         });
+//     },
 
-    update: function(project) {
-      return $http({
-            url: '/api/projects/' + project._id,
-            method: "PUT",
-            data: project
-      })
-        .then(function(_project) {
-          return _project.data;
-        });
-    },
+//     add: function(project) {
+//       return $http({
+//             url: '/api/projects/',
+//             method: "POST",
+//             data: project
+//       })
+//         .then(function(_project) {
+//           return _project.data;
+//         });
+//     },
 
-    getDataSets: function(productId){
-      return null;
-    }
+//     delete: function(id){
+//       return $http.delete('/api/projects/' + id)
+//         .then(function(project) {
+//           return project.data;
+//         });
+//     },
 
-  };
+//     update: function(project) {
+//       return $http({
+//             url: '/api/projects/' + project._id,
+//             method: "PUT",
+//             data: project
+//       })
+//         .then(function(_project) {
+//           return _project.data;
+//         });
+//     },
 
-  return projectObj;
-});
+//     getDataSets: function(productId){
+//       return null;
+//     }
+
+//   };
+
+//   return projectObj;
+// });
 
 
