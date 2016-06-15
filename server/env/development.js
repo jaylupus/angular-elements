4e3c8e41fd6ef4f72f050e9d31161a422decb41b
Jonah Wolf <wolfjonah@gmail.com>
2016-06-14 20:47:32 -0400

    Returned developmentnt file

---
+++ server/env/development.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "DATABASE_URI": "mongodb://localhost:27017/capstone",
+  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "GITHUB": {
+    "clientID": "b97ff1766890c5932d2d",
+    "clientSecret": "ffdb0abf76befe506a45c26c11261223403b8f7d",
+    "callbackURL": "/auth/github/callback"
+  }
+};
