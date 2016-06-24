b605496b168765f9dae19ad224c3b77f6f00cd6b
andrewmenendez <andrew@engrade.com>
2016-06-24 17:13:29 -0400

    file saving working

--- server/app/routes/data/index.js
+++ server/app/routes/data/index.js
@@ -9,6 +9,7 @@ var rootPathStr = __dirname.split('server/app/routes/data');
 var rootPath = rootPathStr[0];
 var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
+var fs1 = require('fs');
 
 
 router.post('/:projId/:userId', function(req, res, next){
@@ -20,34 +21,32 @@ router.post('/:projId/:userId', function(req, res, next){
 		//Path where image will be uploaded
     fstream = fs.createWriteStream(rootPath + '/files/' + filename);
     file.pipe(fstream);
-    fstream.on('close', function () {    
-        console.log("Upload Finished of " + filename);              
-        // res.redirect('/');           //where to go next
-        // fs.readFile(rootPath + '/files/' + filename, 'utf8', function(err, data){
-        // 	if (err){
-        // 		console.log(err);
-        // 	}
-        // 	var dataSource = {
-        // 		fileName: filename,
-        // 		dataType: 'linear',
-        // 		data: data
-        // 	}
-        // 	DataSource.create(dataSource);
-        // })
+    fstream.on('close', function () {
+        console.log("Upload Finished of " + filename); //cool;
+        return new Promise(function(fulfill, reject) {
+            fs1.readFile(rootPath+'/files/'+filename, 'utf8', function(err, res) {
+              if (err) {
+                console.log(err);
+                reject(err);
+              }
+              else fulfill(res);
+            });
+          })
+          .then(function(contents) {
+            converter.fromString(contents, function(err,result){
+                            //your code here
 
-        fs.createReadStream(rootPath + '/files/' + filename).pipe(converter);
+            var dataSource = {
+              fileName: filename,
+              data: JSON.stringify(result)
+            };
 
-        converter.on('end_parsed', function(data) {
-          console.log(data);
-          var dataSource = {
-            user: req.params.userId,
-            project: req.params.projId,
-            fileName: filename,
-            dataType: 'linear',
-            data: JSON.stringify(data)
-          }
-          DataSource.create(dataSource);    
-        });
+            return DataSource.create(dataSource).then(function(dataObj){
+              console.log(dataObj);
+              res.send(dataObj);
+            })
+          });
+          })
 
       });
 	})
