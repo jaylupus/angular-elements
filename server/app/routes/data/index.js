c8d5908f86a9bdfaed92f6b570a4123187bb98e4
flexjames <james.sh.ma@gmail.com>
2016-06-26 12:48:22 -0400

    Fixed Data Uploader Route

--- server/app/routes/data/index.js
+++ server/app/routes/data/index.js
@@ -10,46 +10,102 @@ var rootPath = rootPathStr[0];
 var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
 var fs1 = require('fs');
+var fsp = require('fs-promise');
 
 
 router.post('/:projId/:userId', function(req, res, next){
-	console.log('made it here');
-	var fstream;
-	req.pipe(req.busboy);
-	req.busboy.on('file', function(fieldname, file, filename){
-		console.log('Uploading: ' + filename);
+	// var fstream;
+	// req.pipe(req.busboy);
+ //  console.log('Entering post route...');
+	// req.busboy.on('file', function(fieldname, file, filename){
+	// 	console.log('Uploading: ' + filename);
 		//Path where image will be uploaded
-    fstream = fs.createWriteStream(rootPath + '/files/' + filename);
-    file.pipe(fstream);
-    fstream.on('close', function () {
-        console.log("Upload Finished of " + filename); //cool;
-        return new Promise(function(fulfill, reject) {
-            fs1.readFile(rootPath+'/files/'+filename, 'utf8', function(err, res) {
-              if (err) {
-                console.log(err);
-                reject(err);
-              }
-              else fulfill(res);
-            });
-          })
-          .then(function(contents) {
-            converter.fromString(contents, function(err,result){
-                            //your code here
+    // fstream = fs.createWriteStream(rootPath + '/files/' + filename);
+    // file.pipe(fstream);
+    // fstream.on('close', function () {
+    //   console.log(filename + ' uploaded to files directory'); //cool;
+    //   return new Promise(function(fulfill, reject) {
+    //       fs1.readFile(rootPath+'/files/'+filename, 'utf8', function(err, res) {
+    //         if (err) {
+    //           console.log(err);
+    //           reject(err);
+    //         }
+    //         else fulfill(res);
+    //       });
+    //     })
+    //     .then(function(contents) {
+    //       console.log('contents are ' + contents);
+    //       converter.fromString(contents, function(err,result){
+    //                       //your code here
+    //       console.log('result is ' + result);
+    //       var dataSource = {
+    //         user: req.params.userId,
+    //         project: req.params.projId,
+    //         fileName: filename,
+    //         data: JSON.stringify(result)
+    //       };
 
-            var dataSource = {
-              fileName: filename,
-              data: JSON.stringify(result)
-            };
+    //       return DataSource.create(dataSource).then(function(dataObj){
+    //         console.log('dataOb is ' + dataObj);
+    //         res.status('201').end();
+    //       });
+    //     });
+    //   })
+    // });
 
-            return DataSource.create(dataSource).then(function(dataObj){
-              console.log(dataObj);
-              res.send(dataObj);
-            })
-          });
-          })
+    // fstream = fs.createWriteStream(rootPath + '/files/' + filename);
+    // file.pipe(fstream);
+    // fstream.on('close', function(){
+    //   console.log(filename + ' uploaded to files directory');
+      // fs1.readFile(rootPath + 'files/' + filename, 'utf8', function(err, data){
+      //   if (err) console.log(err);
+      //   converter.fromString(data, function(err,result){
+      //     if (err) console.log(err);
+      //     console.log('result is ' + result);
+      //     var _dataSource = {
+      //       user: req.params.userId,
+      //       project: req.params.projId,
+      //       fileName: filename,
+      //       data: JSON.stringify(result)
+      //     };
+      //     DataSource.create(_dataSource)
+      //     .then(function(dataObj){
+      //       console.log(dataObj);
+      //       res.status('201');
+      //     })
+      //   });
+      // });
+          
+    //   });
+    // });
+  //});
+  console.log('Entering POST route');
+  console.log('req.files is');
+  console.log(req.files);
+  console.log(req.files.file.name);
+  var filePathName = rootPath + 'files/' + req.files.file.name;
+  var filename = req.files.file.name;
+  console.log('filePathName is ' + filePathName);
+  
+  fsp.writeFile(filePathName, req.files.file.data)
+  .then(function(){
+    console.log('hello');
+    return fsp.readFile(filePathName, {encoding: 'utf8'})
+  }).then(function(data){
+    console.log('data is ' + data);
+    var _dataSource = {
+      user: req.params.userId,
+      project: req.params.projId,
+      fileName: filename,
+      data: JSON.stringify(data)
+    };
 
-      });
-	})
+    return DataSource.create(_dataSource)
+    .then(function(dataObj){
+      console.log('dataObj is ' + dataObj);
+      res.send(dataObj);
+    })
+ });
 });
 
 
